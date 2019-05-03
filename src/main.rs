4149e5f71bc2e9743c56227b6e017a691378ef98
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2019-05-03 17:50:50 +0900

    Try to use default scene only when run without parameters

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,18 @@ fn main() {
     let mut args : Vec<String> = std::env::args().collect();
 
     let current_dir = std::env::current_dir().unwrap();
-    println!("Current path: {:?}", std::fs::canonicalize(current_dir));
+    println!("Current path: {:?}", std::fs::canonicalize(current_dir).unwrap());
 
     //current_dir() is the root directory of the project, setting relative paths
-    let scene_file_path = "scenes/default_scene.toml".to_string();
-    args.push(scene_file_path);
+    //If no arguments specified, try to use a default scene
+    if args.len() == 1 {
+        let scene_file_path = "scenes/default_scene.toml".to_string();
+        args.push(scene_file_path);
+    }
     println!("{:?}", args);
-    println!("Executable path is: {:?}", std::env::current_exe());
     let mut scene_config = SceneConfig::default();
 
-    let is_parse_okay = scene_config.parse_args(&args).unwrap_or_else(
+    let _is_parse_okay = scene_config.parse_args(&args).unwrap_or_else(
       |err| {
           eprintln!("Problem parsing scene file: {}", err);
           process::exit(1);
