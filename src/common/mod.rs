812425ae424993c5e83d4d3b037b2b5908398c05
smukherjee2016 <sabyasachimukherjee2006@gmail.com>
2019-05-04 18:56:47 +0900

    Add Ray

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,3 +1,24 @@
 pub use cgmath::prelude::*;
 pub use f64 as fp;
 pub type Color = cgmath::Vector3<fp>;
+pub type Point3 = cgmath::Vector3<fp>;
+pub type Vec3 = cgmath::Vector3<fp>;
+
+
+pub struct Ray {
+    o : Point3,
+    d: Vec3,
+    t : fp,
+    tmax : fp
+}
+
+impl Ray {
+    pub fn new(origin_ : Point3, direction_ : Vec3, t_ : fp, tmax_ : fp) -> Ray{
+        Ray {
+            o: origin_,
+            d: direction_,
+            t: t_,
+            tmax: tmax_
+        }
+    }
+}
