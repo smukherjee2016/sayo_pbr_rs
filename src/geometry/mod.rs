84c686c23949920530e8f5b2edd24afd10f0dbce
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2019-05-06 20:41:56 +0900

    Add &self to check intersection method

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -3,5 +3,5 @@ use crate::common::*;
 mod triangle;
 
 pub trait Geometry {
-    fn check_intersection_and_return_closest_hit(ray : Ray) -> Option<IntersectionInfo>;
+    fn check_intersection_and_return_closest_hit(&self, ray : Ray) -> Option<IntersectionInfo>;
 }
--- src/geometry/triangle.rs
+++ src/geometry/triangle.rs
@@ -10,7 +10,7 @@ struct Triangle {
 }
 
 impl Geometry for Triangle {
-    fn check_intersection_and_return_closest_hit(ray: Ray) -> Option<IntersectionInfo> {
+    fn check_intersection_and_return_closest_hit(&self, ray: Ray) -> Option<IntersectionInfo> {
         let intersection_info = IntersectionInfo {
             t_intersection: 0.0,
             point_of_intersection: ZERO_VECTOR,
