cd31b498ec3fc3d80b6d05361f05d72ccb60399f
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2019-08-16 02:05:49 +0900

    Add methods to Vector2 and Vector3

--- src/utilities/mathutils.rs
+++ src/utilities/mathutils.rs
@@ -1,6 +1,5 @@
 use core::ops;
 pub use f64 as fp;
-use std::cmp::max;
 
 pub type Spectrum = Vector3;
 pub type Point3 = Vector3;
@@ -56,6 +55,17 @@ impl ops::Mul for Vector3 {
     }
 }
 
+impl ops::Mul<fp> for Vector3 {
+    type Output = Self;
+    fn mul(self, rhs: fp) -> Self::Output {
+        Vector3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
 impl ops::Div for Vector3 {
     type Output = Self;
 
@@ -64,6 +74,17 @@ impl ops::Div for Vector3 {
             x: self.x / rhs.x,
             y: self.y / rhs.y,
             z: self.z / rhs.z,
+        }
+    }
+}
+
+impl ops::Div<fp> for Vector3 {
+    type Output = Self;
+    fn div(self, rhs: fp) -> Self::Output {
+        Vector3 {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
         }
     }
 }
@@ -185,11 +206,154 @@ impl Vector3 {
     }
 }
 
+//Utility function to construct a coordinate system by making a 2nd vector
+//perpendicular to it and a 3rd vector perpendicular to both by cross product
+pub fn coordinate_system(in_vec: Vector3, v2: &mut Vector3, v3: &mut Vector3) {
+    let in_vec_normalized: Vector3 = in_vec.normalize();
+    if fp::abs(in_vec_normalized.x) > fp::abs(in_vec_normalized.y) {
+        *v2 = Vector3::new(-in_vec_normalized.z, 0.0, in_vec_normalized.x)
+            / (fp::sqrt(in_vec_normalized.x * in_vec_normalized.x + in_vec_normalized.z * in_vec_normalized.z));
+    }
+    else {
+        *v2 = Vector3::new(0.0, in_vec_normalized.z, -in_vec_normalized.y)
+            / (fp::sqrt(in_vec_normalized.y * in_vec_normalized.y + in_vec_normalized.z * in_vec_normalized.z));
+    }
+    *v3 = in_vec_normalized.cross(v2.clone()).clone();
+}
+
+impl ops::Sub for Vector2 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vector2 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl ops::Add for Vector2 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vector2 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl ops::Mul for Vector2 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Vector2 {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+        }
+    }
+}
+
+impl ops::Mul<fp> for Vector2 {
+    type Output = Self;
+    fn mul(self, rhs: fp) -> Self::Output {
+        Vector2 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+
+impl ops::Div for Vector2 {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Vector2 {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+        }
+    }
+}
+
+impl ops::Div<fp> for Vector2 {
+    type Output = Self;
+    fn div(self, rhs: fp) -> Self::Output {
+        Vector2 {
+            x: self.x / rhs,
+            y: self.y / rhs,
+        }
+    }
+}
+
+impl ops::AddAssign for Vector2 {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl ops::SubAssign for Vector2 {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl ops::MulAssign<fp> for Vector2 {
+    fn mul_assign(&mut self, rhs: fp) {
+        self.x *= rhs;
+        self.y *= rhs;
+    }
+}
+
+impl ops::DivAssign<fp> for Vector2 {
+    fn div_assign(&mut self, rhs: fp) {
+        self.x /= rhs;
+        self.y /= rhs;
+    }
+}
+
 impl Vector2 {
+    pub fn normalize(&self) -> Vector2 {
+        let normalization_factor = (self.x * self.x + self.y * self.y).sqrt();
+        Vector2 {
+            x: self.x / normalization_factor,
+            y: self.y / normalization_factor,
+        }
+    }
+
+    pub fn from(scalar: fp) -> Vector2 {
+        Vector2 {
+            x: scalar,
+            y: scalar,
+        }
+    }
+
     pub fn new(scalar_x: fp, scalar_y: fp) -> Vector2 {
         Vector2 {
             x: scalar_x,
             y: scalar_y,
+        }
+    }
+
+    pub fn permute(&self, x: i32, y: i32) -> Vector2 {
+        let original_vector_elements: Vec<fp> = vec![self.x, self.y];
+
+        Vector2 {
+            x: *original_vector_elements.get(x as usize).unwrap(),
+            y: *original_vector_elements.get(y as usize).unwrap(),
+        }
+    }
+
+   pub fn abs(&self) -> Vector2 {
+        Vector2 {
+            x: fp::abs(self.x),
+            y: fp::abs(self.y),
         }
     }
 }
