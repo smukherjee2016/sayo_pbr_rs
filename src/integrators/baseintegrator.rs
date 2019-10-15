1d9cc9923318e55e57203a4dd51812f543e94ea8
Sabyasachi Mukherjee <smukherjee2016@users.noreply.github.com>
2019-10-15 20:29:14 +0900

    Add non-working scoped threadpool

--- src/integrators/baseintegrator.rs
+++ src/integrators/baseintegrator.rs
@@ -5,6 +5,7 @@ use crate::integrators::Integrator;
 use crate::{SceneConfig, Tile};
 use crossbeam::crossbeam_channel::unbounded;
 use std::borrow::Borrow;
+use scoped_pool::Pool;
 
 pub struct BaseIntegrator;
 
@@ -19,6 +20,8 @@ impl Integrator for BaseIntegrator {
         let mut tiles: Vec<Tile> = vec![];
         let cpus = num_cpus::get();
         info!("Trying with {} cpus", cpus);
+        let pool = Pool::new(cpus);
+
         let film = scene.film.borrow();
         info!(
             "Beginning rendering with {} spp and {} bounces",
@@ -27,10 +30,10 @@ impl Integrator for BaseIntegrator {
         let (s, r) = unbounded();
 
         let pixel_numbers = 0..(film.height * film.width);
-
+        pool.scoped(|scope| {
             for i in pixel_numbers.step_by(TILE_SIZE) {
                 let sender = s.clone();
-                //pool.execute(move ||
+                scope.execute(move ||
                  match scene.integrator {
                     Integrators::DirectLighting => {
                         let tile: Tile = DirectLightingIntegrator::integrate(
@@ -44,12 +47,12 @@ impl Integrator for BaseIntegrator {
                     Integrators::PathTracerBSDF => {}
                     Integrators::PathTracerNEE => {}
                 }
-                //);
+                );
 
                 tiles.push(r.recv().unwrap());
                 //warn!("{:?}", tile.pixels);
             }
-
+        });
         info!("Finished running render()");
         tiles
     }
