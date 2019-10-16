4a80319c641d1059865848706e06214238616b27
Sabyasachi Mukherjee <smukherjee2016@users.noreply.github.com>
2019-10-16 12:16:02 +0900

    Remove mpsc threadpool specific file

--- src/common/mpscthreadpool.rs
+++
@@ -1,6 +0,0 @@
-/*
-    Initial draft idea v0 (20190820): each thread of the scoped threadpool will be a sender,
-    and main thread will be the receiver. Each thread will calculate the final SPP-averaged
-    value of the pixel(s) it is given in charge of. Possibly store the block start positions in
-    the image array and make the threads work on it till all the blocks are exhausted.
-*/
